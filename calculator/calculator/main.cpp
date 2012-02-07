e7c4c9e766513cae6270b4fcd1aa41fe4354834a
Alex Egg <eggie5@gmail.com>
2012-02-06 20:14:37 -0800

    added loop

--- calculator/calculator/main.cpp
+++ calculator/calculator/main.cpp
@@ -3,52 +3,80 @@
 //  calculator
 //
 //  Created by Alex Egg on 1/19/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 All rights reserved.
 //
 
 #include <iostream>
-//using namespace std;
+using namespace std;
 
 int main (int argc, const char * argv[])
 {
-
+    while(true)
+    {
     int l;
     int r;
-    std::string opperand;
-    std::cout << "Please enter first argument ";
-    std::cin >> l;
-    std::cout << "Please enter second argument ";
-    std::cin >> r;
-    std::cout << "Please enter opperand ";
-    std::cin>>opperand;
+    string opperand;
+        
+    cout << "\nPlease enter first argument ";
+    cin >> l;
+    cout << "Please enter second argument ";
+    cin >> r;
+    cout << "Please enter opperand ";
+    cin>>opperand;
     
     if(opperand.compare("+")==0)
     {
-        std::cout << l+r;
+        cout << l+r;
     }
     else if(opperand.compare("-")==0)
     {
-        std::cout << l-r;
+        cout << l-r;
     }
     else if(opperand.compare("*")==0)
     {
-        std::cout << l*r;
+        cout << l*r;
     }
     else if(opperand.compare("/")==0)
     {
-        std::cout << l/r;
+        cout << l/r;
     }
     else if(opperand.compare("%")==0)
     {
-        std::cout << l%r;
+        cout << l%r;
     }
     else
     {
-        std::cout << "unknown opperand";
+        cout << "unknown opperand";
     }
+    }
+    
+    cout<<"\n";
         
     
     
     return 0;
 }
 
+
+//Sample Output
+//Please enter first argument 2
+//Please enter second argument 2
+//Please enter opperand +
+//4
+//Please enter first argument 2
+//Please enter second argument 3
+//Please enter opperand -
+//-1
+//Please enter first argument 3
+//Please enter second argument 4
+//Please enter opperand *
+//12
+//Please enter first argument 10
+//Please enter second argument 3
+//Please enter opperand %
+//1
+
+
+//Discussion Section:
+//
+//This is a very simple program. It sits in an infinite loop waiting for user input of 3 variables. The first, second and arithmetic operand. It then passes the arithmetic operand though a series of string compares to determine which operation to preform on the first and second operands. If no match is found it falls to the else statement where an error is displayed. 
