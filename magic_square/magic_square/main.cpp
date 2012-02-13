9c97f0f95e5480c2d001147daf0ad655806a4247
Alex Egg <eggie5@gmail.com>
2012-02-13 09:06:37 -0800

    added >> 2 instead of /2

--- magic_square/magic_square/main.cpp
+++ magic_square/magic_square/main.cpp
@@ -16,7 +16,7 @@ int main (int argc, const char * argv[])
     int n=5;
     for (int i=1; i<=n; i++) {
         for (int j=1; j<=n; j++) {
-            int val=n*((i+j-1+(int)floor(n/2))%n) + ((i+2*j-2)%n)+1;
+            int val=n*((i+j-1+(int)floor(n>>2))%n) + ((i+2*j-2)%n)+1;
             
             cout << val <<" ";
             if(j%n==0) //newline
