8766f17fe466cb0e1c9f38ab56910c22eb9ff4b8
Alex Egg <eggie5@gmail.com>
2012-04-23 23:13:03 -0700

    removed C project

--- BST/BST/main.cpp
+++ BST/BST/main.cpp
@@ -1,18 +1,18 @@
 //
-//  main.c
+//  main.cpp
 //  BST
 //
 //  Created by Alex Egg on 4/23/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    printf("Hello, World!\n");
+    std::cout << "Hello, World!\n";
     return 0;
 }
 
